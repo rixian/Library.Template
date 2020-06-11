ee86a01eb1a8df07e1b76f372b07270f7b70c94c
Samuel McAravey <SamuelMcAravey@users.noreply.github.com>
2020-06-11 10:00:02 -0700

    Added merged files...

--- Expand-Template.ps1
+++ Expand-Template.ps1
@@ -1,3 +1,5 @@
+#!/usr/bin/env pwsh
+
 <#
 .SYNOPSIS
 Expands this template into an actual project, taking values for placeholders
@@ -80,52 +82,50 @@ try {
     }
 
     # Rename project directories and solution
-    Set-Location src
     git mv Library.sln "$LibraryName.sln"
-    git mv Library/Library.csproj "Library/$LibraryName.csproj"
-    git mv Library "$LibraryName"
-    git mv Library.Tests/Library.Tests.csproj "Library.Tests/$LibraryName.Tests.csproj"
-    git mv Library.Tests "$LibraryName.Tests"
+    git mv src/Library/Library.csproj "src/Library/$LibraryName.csproj"
+    git mv src/Library "src/$LibraryName"
+    git mv test/Library.Tests/Library.Tests.csproj "test/Library.Tests/$LibraryName.Tests.csproj"
+    git mv test/Library.Tests "test/$LibraryName.Tests"
 
     # Refresh solution file both to update paths and give the projects unique GUIDs
-    dotnet sln remove Library/Library.csproj
-    dotnet sln remove Library.Tests/Library.Tests.csproj
-    dotnet sln add "$LibraryName"
-    dotnet sln add "$LibraryName.Tests"
+    dotnet sln remove src/Library/Library.csproj
+    dotnet sln remove test/Library.Tests/Library.Tests.csproj
+    dotnet sln add "src/$LibraryName"
+    dotnet sln add "test/$LibraryName.Tests"
     git add "$LibraryName.sln"
 
     # Update project reference in test project. Add before removal to keep the same ItemGroup in place.
-    dotnet add "$LibraryName.Tests" reference "$LibraryName"
-    dotnet remove "$LibraryName.Tests" reference Library/Library.csproj
-    git add "$LibraryName.Tests/$LibraryName.Tests.csproj"
+    dotnet add "test/$LibraryName.Tests" reference "src/$LibraryName"
+    dotnet remove "test/$LibraryName.Tests" reference src/Library/Library.csproj
+    git add "test/$LibraryName.Tests/$LibraryName.Tests.csproj"
 
     # Establish a new strong-name key
-    & $sn.Path -k 2048 strongname.snk
-    git add strongname.snk
-
-    Set-Location ..
+    & $sn.Path -k 2048 src/strongname.snk
+    git add src/strongname.snk
 
     # Replace placeholders in source files
     Replace-Placeholders -Path "src/$LibraryName/Calculator.cs" -Replacements @{
         'Library'=$LibraryName
         'COMPANY-PLACEHOLDER'=$Author
     }
-    Replace-Placeholders -Path "src/$LibraryName.Tests/CalculatorTests.cs" -Replacements @{
+    Replace-Placeholders -Path "test/$LibraryName.Tests/CalculatorTests.cs" -Replacements @{
         'Library'=$LibraryName
         'COMPANY-PLACEHOLDER'=$Author
     }
     Replace-Placeholders -Path "LICENSE" -Replacements @{
         'COMPANY-PLACEHOLDER'=$Author
     }
-    Replace-Placeholders -Path "src/stylecop.json" -Replacements @{
+    Replace-Placeholders -Path "stylecop.json" -Replacements @{
         'COMPANY-PLACEHOLDER'=$Author
     }
-    Replace-Placeholders -Path "src/Directory.Build.props" -Replacements @{
+    Replace-Placeholders -Path "Directory.Build.props" -Replacements @{
         'COMPANY-PLACEHOLDER'=$Author
     }
     Replace-Placeholders -Path "README.md" -Replacements @{
         "(?m)^.*\[NuGet package\][^`r`n]*"="[![NuGet package](https://img.shields.io/nuget/v/$LibraryName.svg)](https://nuget.org/packages/$LibraryName)"
-        "(?m)^.*\[Build Status\].*`r?`n"=""
+        "(?m)^.*\[Azure Pipelines status\].*`r?`n"=""
+        "(?m)^.*\[GitHub Actions status\].*`r?`n"=""
         "(?m)^.*\[codecov\].*`r?`n"=""
     }
 
@@ -148,7 +148,6 @@ try {
     Replace-Placeholders -Path "azure-pipelines.yml" -Replacements $YmlReplacements
 
     # Self destruct
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
     git rm Expand-Template.*
     git rm :/azure-pipelines/expand-template.yml
 
@@ -176,4 +175,4 @@ try {
 }
 
 # When testing this script, all the changes can be quickly reverted with this command:
-# git reset HEAD :/README.md :/LICENSE :/azure-pipelines.yml :/src :/azure-pipelines; git co -- :/README.md :/LICENSE :/azure-pipelines.yml :/src :/azure-pipelines; git clean -fd :/src
+# git reset HEAD :/README.md :/LICENSE :/azure-pipelines.yml :/src :/test :/azure-pipelines; git co -- :/README.md :/LICENSE :/azure-pipelines.yml :/src :/azure-pipelines; git clean -fd :/src :/test
